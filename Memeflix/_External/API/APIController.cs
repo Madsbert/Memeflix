c9e2c828a841eb11b861f128a7d1e3ec72a8780a
Jakob2rune <jakjes01@easv365.dk>
2025-11-07 11:48:03 +0100

    Created API Controller

---
+++ Memeflix/_External/API/APIController.cs
@@ -0,0 +1,32 @@
+﻿using Memeflix.____Domain;
+using Memeflix.___Application.Interfaces;
+using Memeflix.__Gateway;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Memeflix._External.API;
+
+[ApiController]
+[Route("api/[controller]")]
+public class APIController :ControllerBase
+{
+    
+    public readonly IMovieService _movieService;
+    
+    public APIController(IMovieService movieService)
+    {
+        _movieService = movieService;
+    }
+
+    [HttpPost("uploadMovie")]
+    public async Task<IActionResult> UploadMovieAsync([FromForm] MovieUploadModel file)
+    {
+        if (file == null || file.Moviefile.Length == 0)
+        {
+           return BadRequest("No file provided");
+        }
+        
+        var fileId = await _movieService.UploadMovieAsync(file.Moviefile);
+        return Ok(fileId);
+    }
+    
+}
