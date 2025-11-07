4a622843399a20d0772dd780f1639f8a9cd12feb
Jakob2rune <jakjes01@easv365.dk>
2025-11-07 10:24:53 +0100

    bugfix

---
+++ Memeflix/___Application/Services/MovieServe.cs
@@ -0,0 +1,17 @@
+﻿using Memeflix.___Application.Interfaces;
+using MongoDB.Bson;
+
+namespace Memeflix.___Application.Services;
+
+public class MovieSection : IMovieService
+{
+    public Task<ObjectId> UploadMovieAsync(IFormFile file)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<byte[]> DownloadMovieAsync(ObjectId fileId)
+    {
+        throw new NotImplementedException();
+    }
+}
